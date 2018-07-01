6c0a37de9b19740f4713726963ae755a62b0f81a
dBenov <benov95@abv.bg>
2018-07-02 01:01:35 +0300

    Refactoring CommandManager.

--- God/CommandManager.cs
+++ God/CommandManager.cs
@@ -6,99 +6,97 @@ using System.Threading.Tasks;
 
 namespace God
 {
+   // This class parses and executes user's commands. 
+   ///Add Planet syntax: <add>|<name_of_new_planet>
+   ///Add Entities to a Planet syntax: <add> <name_planet> <entity|animal|human|god> <number_of_creatures>
+   ///Kill creatures of the planet syntax:<kill>|<name_planet>
+   ///Destroy entire planet syntax: <destroy>|<name_planet> 
+   ///Statistic syntax: <statistic>
     public static class CommandManager
     {
+        // Command Add for adding planets or entites to planets.
+        public static void ActionAdd(string[] commands) 
+        {
+            // Add planet.
+            if (commands.Length == 3 && commands[1] == "planet")
+               {
+                  Planet m = new Planet(commands[2]);
+                  Scene.AllPlanets.Add(m);
+                  Console.WriteLine("You add planet:{0}", Scene.AllPlanets.ElementAt(Scene.AllPlanets.Count - 1).Name);
+               }
+
+            // Add creature to a planet.
+            if(commands.Length==4)
+            {
+               for (int i = 0; i < Scene.AllPlanets.Count(); i++)
+               {    
+                  if (Scene.AllPlanets[i].Name == commands[1])
+                  {
+                     int m = int.Parse(commands[3]);
+                     EntityType type = (EntityType)Enum.Parse(typeof(EntityType), commands[2]);                         
+                     for (int k = 0; k < m; k++)
+                     {
+                        Scene.CreateEntity(type, Scene.AllPlanets[i]);
+                     }     
+                  }
+               }
+               Console.WriteLine("You added to planet:{0},{1} {2}", commands[1], commands[3], commands[2]);
+            }
+        }
         public static void Read()
         {
           string command = Console.ReadLine();
-
-          string [] commands=command.Split(' ');
+          string [] commands = command.Split(' ');
 
           switch(commands[0])
           {
 
             case "add":
-                ///Add Planet syntax: <add>|<name_of_new_planet>
-
-
-                if (commands.Length == 3 && commands[1] == "planet")
-                {
-                    Planet m = new Planet(commands[2]);
-                    Scene.AllPlanets.Add(m);
-                    Console.WriteLine("You add planet:{0}", Scene.AllPlanets.ElementAt(Scene.AllPlanets.Count - 1).Name);
-                }
-                ///Add entity of specific type and specific numbers of that kind of entity
-                ///syntax:<add> <name_planet> <entity|animal|human|god> <number_of_creatures>, 
-                if(commands.Length==4)
-                {
-                   for (int i = 0; i < Scene.AllPlanets.Count(); i++)
-             {
-
-                       if (Scene.AllPlanets[i].Name == commands[1])
-                       {
-                           int m = int.Parse(commands[3]);
-                           EntityType type = (EntityType)Enum.Parse(typeof(EntityType), commands[2]);
-                          
-                               for (int k = 0; k < m; k++)
-                               {
-                                   Scene.CreateEntity(type, Scene.AllPlanets[i]);
-                               }
-                           
-                           
-                       }
-                      
-                   
-                     }
-                    Console.WriteLine("You add to planet:{0},{1} {2}", commands[1], commands[3], commands[2]);
-                 }
+                ActionAdd(commands);
                 break;
 
-            ///Kill creatures of the planet syntax:<kill>|<name_planet>
             case "kill":
-                 foreach (Planet it in Scene.AllPlanets)
+               foreach (Planet planet in Scene.AllPlanets)
+               {
+                  if (planet.Name == commands[1])
                   {
-                      if (it.Name == commands[1])
-                      {
-                          it.citizens.Clear();
-                      }
+                     planet.citizens.Clear();
                   }
-                 Console.WriteLine("You kill creatures of planet:{0}", commands[1]);
-                break;
+               }
+               Console.WriteLine("You kill creatures of planet:{0}", commands[1]);
+               break;
 
-           ///destroy entire planet syntax: <destry>|<name_planet>
+           
             case "destroy":
-                for (int i = 0; i < Scene.AllPlanets.Count; i++)
-                {
-                    if(Scene.AllPlanets[i].Name==commands[1])
-                    {
-                        Scene.AllPlanets.RemoveAt(i);
-                        Console.WriteLine("You destroyed planet:{0}", commands[1]);
-                    }
-                   
-                }
-               
-                  
-                break;
-            /// statistic syntax: <statistic>
+               for (int i = 0; i < Scene.AllPlanets.Count; i++)
+               {
+                  if(Scene.AllPlanets[i].Name==commands[1])
+                  {
+                     Scene.AllPlanets.RemoveAt(i);
+                     Console.WriteLine("You destroyed planet:{0}", commands[1]);
+                  }              
+               }                 
+            break;
+
             case "statistic":
                 if (Scene.AllPlanets.Count != 0)
                 {
                     Console.WriteLine(Scene.AllPlanets.Count());
                     foreach (var planet in Scene.AllPlanets)
-                    {
-                        
+                    {  
                         Console.WriteLine(planet);
                         foreach (var cit in planet.citizens)
                         {
-                            Console.WriteLine(cit);
+                           Console.WriteLine(cit);
                         }
                     }
                 }
                 else
                 {
-                    Console.WriteLine("there is not planets created");
+                    Console.WriteLine("there are no planets created");
                 }
                 break;
+
             case " ":
                 break;
 
@@ -106,10 +104,8 @@ namespace God
                 Console.WriteLine("Wrong command");
                 break;
          }
-
-            CommandManager.Read();
-        
+         
+         CommandManager.Read();        
         }
-
     }
 }
